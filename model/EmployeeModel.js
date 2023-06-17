90421baede221c34044f713a777330cc63859090
José Sanches Malassise <92497975+zesanches@users.noreply.github.com>
2023-06-17 00:07:35 -0300

    fix employee model

--- model/EmployeeModel.js
+++ model/EmployeeModel.js
@@ -3,67 +3,78 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 class EmployeeModel {
-    async getAllEmployees() {
-        let employees = await prisma.$queryRaw`
-            SELECT employee.*, pontos.*
+  async getAllEmployees() {
+    let returns = [];
+    let employees = await prisma.$queryRaw`
+            SELECT employee.* pontos.*
             FROM employee
-            JOIN pontos ON employee.id = pontos.employee_id;
         `;
 
-        return employees;
+    if (employees?.length) {
+      for (emp of employees) {
+        let pontos =
+          await prisma.$queryRaw`SELECT * FROM pontos WHERE employee_id = ${emp.id}`;
+
+        returns.push({ ...emp, pontos });
+      }
     }
 
-    async createEmployee(name, is_pj, employer_id) {
-        let employee = await prisma.employee.create({
-            data: {
-                name,
-                is_pj,
-                employer_id
-            }
-        }).catch((err) => {
-            console.log(err);
-        });
+    return returns;
+  }
 
-        return employee;
-    }
+  async createEmployee(name, is_pj, employer_id) {
+    let employee = await prisma.employee
+      .create({
+        data: {
+          name,
+          is_pj,
+          employer_id,
+        },
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-    async updateEmployee(id, name, email, phone) {
-        let employee = await prisma.employee.update({
-            where: {
-                id: id
-            },
-            data: {
-                name: name,
-                email: email,
-                phone: phone
-            }
-        });
+    return employee;
+  }
 
-        return employee;
-    }
+  async updateEmployee(id, name, email, phone) {
+    let employee = await prisma.employee.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name: name,
+        email: email,
+        phone: phone,
+      },
+    });
 
-    async deleteEmployee(id) {
-        let employee = await prisma.employee.delete({
-            where: {
-                id: id
-            }
-        });
+    return employee;
+  }
 
-        return employee;
-    }
+  async deleteEmployee(id) {
+    let employee = await prisma.employee.delete({
+      where: {
+        id: id,
+      },
+    });
 
-    async getEmployee(id) {
-        let employee = await prisma.employee.findUnique({
-            where: {
-                id: id
-            },
-            include: {
-                pontos: true
-            }
-        });
+    return employee;
+  }
 
-        return employee;
-    }
+  async getEmployee(id) {
+    let employee = await prisma.employee.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        pontos: true,
+      },
+    });
+
+    return employee;
+  }
 }
 
 module.exports = new EmployeeModel();
