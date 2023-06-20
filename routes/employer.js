f897a3a35888342a0fbe6cf2117aa420a4f5c01e
paixaodaniel <75765224+paixaodaniel@users.noreply.github.com>
2023-06-20 17:54:53 -0300

    Feat Employer

--- routes/employer.js
+++ routes/employer.js
@@ -1,25 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const EmployerController = require('../controller/EmployerController.js');
+const EmployerController = require("../controller/EmployerController.js");
 
-router.get('/', function(req, res, next) {
-  EmployerController.listEmployers(req, res)
+router.get("/", function (req, res, next) {
+  EmployerController.listEmployers(req, res);
 });
 
-router.post('/create', function(req, res, next) {
-  EmployerController.createEmployer(req, res)
+router.post("/create", function (req, res, next) {
+  EmployerController.createEmployer(req, res);
 });
 
-router.post('/update', function(req, res, next) {
-  EmployerController.updateEmployer(req, res)
+router.post("/update", function (req, res, next) {
+  EmployerController.updateEmployer(req, res);
 });
 
-router.post('/delete', function(req, res, next) {
-  EmployerController.deleteEmployer(req, res)
+router.post("/delete", function (req, res, next) {
+  EmployerController.deleteEmployer(req, res);
 });
 
-router.post('/get', function(req, res, next) {
-  EmployerController.getEmployer(req, res)
+router.post("/get", function (req, res, next) {
+  EmployerController.getEmployer(req, res);
+});
+
+router.post("/points", function (req, res, next) {
+  EmployerController.getPoints(req, res);
+});
+
+router.put("/point/:employee", function (req, res, next) {
+  EmployerController.adjustPoint(req, res);
 });
 
 module.exports = router;
